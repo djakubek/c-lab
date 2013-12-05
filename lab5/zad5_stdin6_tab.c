4cf7724f6d2f7bdd398398dafd928e4a1715cbae
Daniel Jakubek <k.a.s.z.u.b@wp.pl>
2013-12-05 13:16:42 +0100

    Program wczytujacy liczby do tab i wypisujacy je w odwrotnej kolejnosci

---
+++ lab5/zad5_stdin6_tab.c
@@ -0,0 +1,28 @@
+/*Program, który wczyta ze stdin 6 liczb 
+całkowitych do tablicy, a następnie wypisze
+ zawartość tej tablicy od początku i od końca*/
+ 
+#include <stdio.h>
+
+int main()
+{
+  int tab[6];
+  int i;
+
+  i = 0;
+  while (i < 6)
+  {
+    printf("Podaj liczbę: ");
+    scanf("%d", &tab[i]);
+    ++i;
+  }
+  for (i = 0; i < 6; i++)
+  {
+  printf("%d\n", tab[i]);
+  }
+  for (i = 6; i > 0; i--)
+  {
+  printf("%d\n", tab[i]);
+  }
+  return 0;
+}
