a39ec9442c379b934960236a83aef230e654959d
Daniel Jakubek <k.a.s.z.u.b@wp.pl>
2013-12-08 11:16:35 +0100

    Poprawki

--- lab7/zad1_przeciwprostokatna.c
+++ lab7/zad1_przeciwprostokatna.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
+double przeciwprost(double a, double b){
+double wynik;
+    wynik = sqrt(a * a + b * b);
+    return wynik;
+}
 
-double przeciwprost (double a, double b); /*deklaracja funkcji przeciwprost*/
-
-int main (void){
+int main(void){
     double bok1, bok2, wynik;
     printf("podaj dlugosc pierwszego boku: ");
     scanf("%lf", &bok1);
     printf("podaj dlugosc drugego boku: ");
     scanf("%lf", &bok2);
     wynik = przeciwprost(bok1, bok2);
-    printf("%lf \n", wynik);
-return(0);
+    printf("dlugosc przeciwprostokatnej: %lf \n", wynik);
+return 0;
 }
 
-double przeciwprost (double a, double b){
-double wynik;
-    wynik = sqrt(a * a + b * b);
-    
-    return wynik;
-}
+
+/*blad ! */
