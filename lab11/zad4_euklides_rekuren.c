a762d4302282d68118e352907bedda278d6e15dc
Daniel Jakubek <k.a.s.z.u.b@wp.pl>
2013-12-15 12:26:15 +0100

    Euklides NWD reukrencja

---
+++ lab11/zad4_euklides_rekuren.c
@@ -0,0 +1,27 @@
+/* Korzystając z twierdzenia Euklidesa, napisać program
+obliczający rekurencyjnie największy wspólny dzielnik
+z liczb podanych na wejściu 4*/
+
+#include <stdio.h>
+
+int dzielnik(int x, int y);
+
+int main(void) {
+  int liczba1, liczba2, nwd;
+
+  printf("Podaj pierwszą liczbę: ");
+  scanf("%d", &liczba1);
+  printf("Podaj drugą liczbę: ");
+  scanf("%d", &liczba2);
+
+  nwd = dzielnik(liczba1, liczba2);
+  printf("Największy wspólny dzielnik: %d\n", nwd);
+  return nwd;
+}
+
+int dzielnik(int x, int y) {
+
+  if (x == 0)
+    return y;
+  return dzielnik(y % x, x);
+}
