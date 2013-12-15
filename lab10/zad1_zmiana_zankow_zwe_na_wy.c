183c8cbc4662cbbb4c38dc42648b99a684ed51cc
Daniel Jakubek <k.a.s.z.u.b@wp.pl>
2013-12-15 12:59:51 +0100

    Zmiana znakow tabulacji i /

---
+++ lab10/zad1_zmiana_zankow_zwe_na_wy.c
@@ -0,0 +1,34 @@
+/* W poniższym programie, który kopiując wejście
+ na wyjście zastępuje kazdy znak tabulacji 
+ przez \t oraz każdy znak \ przez dwa takie znaki,
+  zostały przestawione wiersze. Uporzadkuj je, 
+  tak aby powstał działający program
+
+#include <stdio.h>
+while ((c=getchar())!=EOF)
+}
+printf("\\t");
+else if (c=='\\')
+printf("\\\\");
+if (c=='\t')
+else
+int c;
+putchar(c);
+int main() {
+return 0;
+*/
+//POPRAWIONY 
+#include <stdio.h>
+int main() {
+int c;
+
+while ((c=getchar())!=EOF)
+if (c=='\t')
+printf("\\t");
+else
+if (c=='\\')
+printf("\\\\");
+else
+putchar(c);
+return 0;
+}
