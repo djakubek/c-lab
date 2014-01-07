82321f2a72da2ef92a19eba4aab9d7eb491aa8de
Daniel Jakubek <k.a.s.z.u.b@wp.pl>
2014-01-07 16:10:15 +0100

    Odwracanie kolejnosci ver 2 -->Funkcje

---
+++ lab7/zad4_odwracanie_kolejnosci_ver2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void inverted(int x);
+
+int main() {
+  int a;
+
+  printf("Wprowadź liczbę: ");
+  scanf("%d", &a);
+  printf("Cyfry od końca:\n");
+  inverted(a);
+  printf("\n");
+
+  return 0;
+}
+
+void inverted(int x) {
+  char *znaki = malloc(23); // malloc(sizeof long long int);
+  /* x -- liczba ==> napis */
+  sprintf(znaki, "%d", x); // sprintf() ==> dostaniemy tę liczbę w napisie
+  int len = strlen(znaki);
+
+  printf("Liczba cyfr: %d\n", len);
+
+  while (len >= 0) { // (długość > 0)
+    printf("%c", znaki[len]);
+    len--;
+  }
+}
+
